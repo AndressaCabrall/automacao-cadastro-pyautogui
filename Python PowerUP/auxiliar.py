8551a003c2ab93bb08e3d01beb77661da6deaf35
Andressa Cabral 👩‍💻 <andressagclima@gmail.com>
2025-06-07 20:44:36 -0300

    algumas alterações no codigo

--- Python PowerUP/auxiliar.py
+++ Python PowerUP/auxiliar.py
@@ -5,15 +5,4 @@ time.sleep(5)
 
 print(pyautogui.position())
 
-from pytube import YouTube
-import os
-url = "https://www.youtube.com/watch?v=9olENDPTyxM"
-
-video = YouTube(url)
-stream = video.streams.get_highest_resolution()
-stream.download()
-
-print("Download concluído!")
-
-
 
--- Python PowerUP/codigo.py
+++ Python PowerUP/codigo.py
@@ -5,10 +5,8 @@ import time
 pyautogui.PAUSE = 1
 
 # Passo 01 -Entrar no sistema da empresa
-#abrir o chome
-
-pyautogui.press("win")
-pyautogui.write("chrome")
+   
+pyautogui.press("win")         
 pyautogui.press("enter")
 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
@@ -16,9 +14,9 @@ pyautogui.press("enter")
 
 time.sleep(1)
 
-# Passo 02- Fazer o login com usuário e senha e botão de entrar
+# Passo 02- Fazer o login no site com usuario e senha   1
 
-pyautogui.click(582,507)
+pyautogui.click(608, 465)
 pyautogui.write("cabrallnegociosonline@gmail.com")
 
 pyautogui.press("tab")
@@ -35,53 +33,83 @@ import pandas
 
 tabela = pandas.read_csv("Python PowerUP/produtos.csv")
 
-# Passo 04 -Cadastrar os Produtos
+print(tabela)
 
 for linha in tabela.index:
 
-
-    pyautogui.click(602, 358)
-
+    pyautogui.click(604, 313)
     codigo = str(tabela.loc[linha, "codigo"])
     pyautogui.write(codigo)
-    
-    pyautogui.press("tab")  
+    pyautogui.press("tab")
+
     marca = str(tabela.loc[linha, "marca"])
     pyautogui.write(marca)
-    
     pyautogui.press("tab")
+
     tipo = str(tabela.loc[linha, "tipo"])
     pyautogui.write(tipo)
-   
-    pyautogui.press("tab")  
-    categoria = str(tabela.loc[linha, "categoria"])
-    pyautogui.write(categoria)
-
     pyautogui.press("tab")
+
+    categoria = str(int(tabela.loc[linha, "categoria"]))
+    pyautogui.write(categoria)
+    pyautogui.press("tab")
+
     preco_unitario = str(tabela.loc[linha, "preco_unitario"])
     pyautogui.write(preco_unitario)
-
-
     pyautogui.press("tab")
-    custo = str(tabela.loc[linha, "custo"])
+
+    custo =str(tabela.loc[linha, "custo"])
     pyautogui.write(custo)
+    pyautogui.press("tab")
 
-    pyautogui.press("tab") 
-    obs = str(tabela.loc[linha, "obs"])
-
+    obs = str(tabela.loc[linha, "obs"]) 
     if obs != "nan":
-              
+             
         pyautogui.write(obs)
 
-    time.sleep(1)
-
-# Clica duas vezes diretamente no botão Enviar, nas coordenadas certas
-
-    pyautogui.doubleClick(863, 667)
+    pyautogui.press("tab")
     pyautogui.press("enter")
 
-# voltar ao inicio da tela
-    pyautogui.scroll(10000)
+    pyautogui.press("home")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+       
+
+   
+    
+   #pyautogui.press("enter")
+
+    #pyautogui.press("home")
+
+
+# Passo 04 -Cadastrar os Produtos
+
+
+
+    
+   
 
 
 
